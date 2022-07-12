edb76df42c8c441610fc164de746d75eb69ecdfd
Your KalBABS <babatundetolulope96@gmail.com>
2022-07-12 14:40:11 -0700

    updated 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,9 @@
-julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra 101-keygen.c -o 101-keygen
-julien@ubuntu:~/0x04$ ./101-crackme "`./101-keygen`"
-Tada! Congrats
-julien@ubuntu:~/0x04$
+#import "main.h"
+/**
+ * main -
+ * Return:
+ */
+int main(void)
+{
+	return (0);
+}
